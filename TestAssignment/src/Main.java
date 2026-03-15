5e7387723ba190ea961ec079a7e2a2017b439fc4
La Gia Phong <lagiaphong.2001phong@gmail.com>
2026-03-15 16:24:31 +0700

    init data bill

--- TestAssignment/src/Main.java
+++ TestAssignment/src/Main.java
@@ -1,5 +1,24 @@
+import main.models.Bill;
+import main.repository.BillRepository;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        BillRepository billRepository = new BillRepository();
+
+        initBillsData(billRepository);
+    }
+
+
+    private static void initBillsData(BillRepository repo) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        repo.add(new Bill(1, "ELECTRIC", 200000, LocalDate.parse("25/10/2020", formatter), "EVN"));
+        repo.add(new Bill(2, "WATER", 175000, LocalDate.parse("30/10/2020", formatter), "SAVACO"));
+        repo.add(new Bill(3, "INTERNET", 800000, LocalDate.parse("30/11/2020", formatter), "VNPT"));
     }
 }
